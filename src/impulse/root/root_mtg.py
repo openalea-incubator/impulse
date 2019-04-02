caa4bec2576d70fb26e16feca0357d06c51fe608
pradal <christophe.pradal@inria.fr>
2019-04-02 15:15:57 +0200

    up

--- src/impulse/root/root_mtg.py
+++ src/impulse/root/root_mtg.py
@@ -5,21 +5,77 @@ from openalea.mtg import turtle as turt
 from openalea.mtg.plantframe import color
 import openalea.plantgl.all as pgl
 
+
+"""
+apex:
+- radius
+- hexose
+- length
+"""
+
+def apex_init(apex):
+    nid = apex
+    nid.length = 0.
+    nid.radius = 0.1
+    nid.hexose = 0.
+    nid.dist_to_ramif = 0.
+    nid.time_burst = 0.
+    nid.order = 0
+    nid.radius_max = 0.1
+
+# Parameters
+LengthSegment = 1.
+DistanceRamif = 2
+Delay = 3
+
+def apex_development(apex, dt, dl, sugar):
+
+    new_apex = []
+    apex.dist_to_ramif += dl
+
+    if apex.time_burst > 0:
+        apex.time_burst -= dt
+        new_apex.append(apex)
+        return new_apex
+
+    if apex.dist_to_ramif >= DistanceRamif:
+        ramif = apex.add_child(edge_type='+', label='Apex',
+                             length=0.,
+                             radius = apex.radius*0.7,
+                             time_burst=Delay,
+                             dist_to_ramif = 0.,
+                             order=apex.order+1)
+        new_apex.append(ramif)
+        apex.dist_to_ramif = 0.
+    else:
+        apex.dist_to_ramif += dl
+
+    if apex.length < LengthSegment+dl:
+        apex.length += dl
+    else:
+        length = apex.length
+        apex.length = LengthSegment
+        apex.label = 'Segment'
+
+        apex = apex.add_child(edge_type='<', label='Apex',
+                             length=length+dl-LengthSegment,
+                             radius = apex.radius,
+                             time_burst=0.,
+                             dist_to_ramif = apex.dist_to_ramif,
+                             order = apex.order)
+    new_apex.append(apex)
+    return new_apex
+
+
 def mtg_root(radius = 0.1):
     g = MTG()
     root = g.add_component(g.root, label='Apex')
     nid = g.node(root)
-    nid.length = 0.
-    nid.radius = radius
-    nid.dist_to_ramif = 0.
-    nid.time_burst = 0.
-
+    apex_init(nid)
     return g
 
+
 class Simulate(object):
-    length_segment = 1
-    distance_ramif = 10
-    delay = 3
 
     def __init__(self, g):
         """ Simulate on MTG. """
@@ -27,41 +83,15 @@ class Simulate(object):
         self._apex = [g.node(v) for v in g.vertices_iter(scale=1) if g.label(v)=='Apex']
 
 
-    def step(self, dt=1, dl = 0.1):
+    def step(self, dt=1., dl = 0.1):
         g = self.g
         apices = list(self._apex)
         self._apex = []
+        sugar = 0.5
         for apex in apices:
-            apex.dist_to_ramif += dl
-
-            if apex.time_burst > 0:
-                apex.time_burst -= dt
-                self._apex.append(apex)
-                continue
-
-            if apex.dist_to_ramif >= self.distance_ramif:
-                ramif = apex.add_child(edge_type='+', label='Apex',
-                                     length=0.,
-                                     radius = apex.radius*0.7,
-                                     time_burst=self.delay,
-                                     dist_to_ramif = 0.)
-                self._apex.append(ramif)
-                apex.dist_to_ramif = 0.
-            else:
-                apex.dist_to_ramif += dl
-
-            if apex.length < self.length_segment+dl:
-                apex.length += dl
-            else:
-                length = apex.length
-                apex.length = self.length_segment
-                apex.label = 'Segment'
-
-                apex = apex.add_child(edge_type='<', label='Apex',
-                                     length=length+dl-self.length_segment,
-                                     radius = apex.radius,
-                                     dist_to_ramif = apex.dist_to_ramif)
-            self._apex.append(apex)
+            new_apices = apex_development(apex, dt=dt, dl=dl, sugar=sugar)
+            self._apex.extend(new_apices)
+            print(self._apex)
 
 
 
